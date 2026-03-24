50f243f2d64fa30c88afeec2419b49f7d66ed24d
이승민 <iseungmin@SeungMin-MacBookPro.local>
2026-03-24 16:31:06 +0900

    DB setting

---
+++ src/main/java/com/seungmin/cozyDelivery/CozyDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.seungmin.cozyDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- TRUNCATE TABLE product;
+-- TRUNCATE TABLE orders;
+
+INSERT INTO product (name, price, quantity)
+VALUES ('치킨', 18000, 10);
+
+INSERT INTO product (name, price, quantity)
+VALUES ('피자', 25000, 5);
